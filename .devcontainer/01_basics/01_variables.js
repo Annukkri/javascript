1a03e7189a9056334752bde9abbf4b5b72a76fe2
Annukkri <annustmk@gmail.com>
2026-07-26 09:14:05 +0000

    Variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID = 1456786
+let accountEmail = "annustmk@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bihar"
+
+// accountID = 456
+
+accountEmail = "ncmst@hc.com"
+accountPassword = "234567"
+accountCity = "Bengaluru"
+console.log(accountCity);

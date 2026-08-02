d43dfe993ff2643ccdb1af9f4ae6e4d7e1152fd1
Annukkri <annustmk@gmail.com>
2026-08-02 08:25:18 +0000

    object in JS(02)

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,29 @@
+const tinderUser = {
+    id : "123abc",
+    name : "Sammy",
+    isLoggedIn : false
+}
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Annu",
+            lastname : "Kumari",
+        }
+    }
+}
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1 : "a" , 2 : "b"}
+const obj2 = {3 : "a" , 4 : "b"}
+// const obj3 = {obj1 , obj2}
+// console.log(obj3);
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));

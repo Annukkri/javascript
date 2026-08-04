3b1e5a7b28a827502089011fa352e58f7eb1f4c9
Annu Kumari <annustmk@gmail.com>
2026-08-04 08:32:59 +0000

    object in JS(03)

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -1,29 +1,39 @@
-const tinderUser = {
-    id : "123abc",
-    name : "Sammy",
-    isLoggedIn : false
-}
-// console.log(tinderUser);
+// const tinderUser = {
+//     id : "123abc",
+//     name : "Sammy",
+//     isLoggedIn : false
+// }
+// // console.log(tinderUser);
 
-const regularUser = {
-    email : "some@gmail.com",
-    fullname : {
-        userfullname : {
-            firstname : "Annu",
-            lastname : "Kumari",
-        }
-    }
-}
+// const regularUser = {
+//     email : "some@gmail.com",
+//     fullname : {
+//         userfullname : {
+//             firstname : "Annu",
+//             lastname : "Kumari",
+//         }
+//     }
+// }
 // console.log(regularUser.fullname);
 // console.log(regularUser.fullname.userfullname);
 // console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 = {1 : "a" , 2 : "b"}
-const obj2 = {3 : "a" , 4 : "b"}
-// const obj3 = {obj1 , obj2}
-// console.log(obj3);
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// const obj1 = {1 : "a" , 2 : "b"}
+// const obj2 = {3 : "a" , 4 : "b"}
+// // const obj3 = {obj1 , obj2}
+// // console.log(obj3);
+// const obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+const course = {
+    coursename : "JS in Hindi",
+    price : "999",
+    courseInstructor: "hitesh"
+}
+//const{courseInstructor} = course
+const{courseInstructor : Instructor} = course
+// console.log(courseInstructor);
+console.log(Instructor);

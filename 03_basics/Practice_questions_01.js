e6f5fb8717e59ceecdd4b2f79485016237408b31
Annu Kumari <annustmk@gmail.com>
2026-08-05 07:46:23 +0000

    Practice Questions

---
+++ 03_basics/Practice_questions_01.js
@@ -0,0 +1,76 @@
+// 1. Area of a circle
+
+// let radius = 4
+// let area = Math.PI*radius*radius
+// console.log("Area of circle : " , area);
+
+// 2 Swap of Two Numbers
+
+// let a = 5
+// let b = 8
+// console.log("Before Swapping");
+// console.log("a = " , a);
+// console.log("b = " , b);
+// let temp = a
+// a = b
+// b = temp
+// console.log("After Swapping");
+// console.log("a = " , a);
+// console.log("b = " , b);
+
+// 3. celsius to Fahrenheit
+
+// let C = 45
+// let F = (C * 9/5) + 32
+// console.log("Fahrenheit : " , F);
+
+// 4. Mini Calculator
+
+// let a = 3
+// let b = 8
+// console.log("Addition : " , a + b);
+// console.log("Subtraction : " , a - b);
+// console.log("Multiplication : " , a * b);
+// console.log("Division : " , a / b);
+// console.log("Modulus : " , a % b);
+
+//5. Calculate Percentage
+// let M = 99
+// let C = 98
+// let P = 97
+// let B = 95
+// let A = 94
+
+// let total = M+C+P+B+A
+// let percentage = (total/500) * 100
+// console.log("Total marks : " , total);
+// console.log("Percentage :  " , percentage + "%"); 
+
+// 6.  Even or Odd
+
+let num = 7
+if (num % 2 == 0){
+    console.log(num  + "is even");
+}
+else{
+ console.log(num  + "is odd");   
+}
+
+// 7. Positive, Negative or Zero
+
+// let num = -5
+// if(num > 0){
+//     console.log(num + "is Positive");
+// }
+// else if(num < 0){
+//    console.log(num + "is Negative");
+// }
+// else {
+//  console.log(num + "is Zero");   
+// }
+
+
+
+
+
+

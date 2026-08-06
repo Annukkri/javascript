36fd5be28abc0b4ac33518c0cb97ea09560e922b
Annu Kumari <annustmk@gmail.com>
2026-08-06 08:29:42 +0000

    scopes in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,21 +1,21 @@
-function sayMyName() {
-    console.log("A");
-    console.log("N");
-    console.log("N");
-    console.log("U");   
-}
-sayMyName()
+// function sayMyName() {
+//     console.log("A");
+//     console.log("N");
+//     console.log("N");
+//     console.log("U");   
+// }
+// sayMyName()
 
-function addTwoNumbers(number1,number2){
-    console.log(number1 + number2 );
-}
-addTwoNumbers(5,2)
-addTwoNumbers()
-addTwoNumbers(3,"a")
-addTwoNumbers(3,"4")
-addTwoNumbers(3,null)
-const result = addTwoNumbers(1,3)
-console.log(result);
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2 );
+// }
+// addTwoNumbers(5,2)
+// addTwoNumbers()
+// addTwoNumbers(3,"a")
+// addTwoNumbers(3,"4")
+// addTwoNumbers(3,null)
+// const result = addTwoNumbers(1,3)
+// console.log(result);
 
 
 // function addTwoNumbers(number1,number2){
@@ -23,9 +23,15 @@ console.log(result);
 //     return result
 // }
 
-// const result = addTwoNumbers(1,3)
+// const result = addTwoNumbers(1,3).devcontainer
 //  console.log("Result : " , result);
 
 
 
 
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+console.log("5" - 2 );
+console.log("5" * 2 );
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// var a = 1;
+// let b= 2;
+// const c = 3;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var f = 30;
+
+if(true){
+    let d = 4;
+    const e = 5;
+    var f = 6;
+}
+// console.log(d);
+// console.log(e);
+console.log(f);

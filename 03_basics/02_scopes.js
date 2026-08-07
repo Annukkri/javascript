8ac66c7d980cfa7c1964a600b664cd32726f8503
Annu Kumari <annustmk@gmail.com>
2026-08-07 09:01:33 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,11 +7,61 @@
 
 // var f = 30;
 
-if(true){
-    let d = 4;
-    const e = 5;
-    var f = 6;
-}
+// if(true){
+//     let d = 4;
+//     const e = 5;
+//     var f = 6;
+// }
 // console.log(d);
 // console.log(e);
-console.log(f);
+// console.log(f);  
+
+// function One(){
+//     const username = "Annu"
+//     function two(){
+//         const website = "Youtube"
+//         console.log(username);
+//         console.log(website);
+//     }
+//     // console.log(website); it will show an error
+//     console.log(username);
+//     two()
+// }  
+// One() 
+
+
+if(true){
+    const username = "Annu"
+    if(username == "Annu"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+// console.log(website); It will show an error
+}
+// console.log(username);  It will show an error
+
+
+// ******************Interesting Facts********************
+function addone(num){
+    return num + 1
+}
+// console.log(addone(4));  It will give the output
+
+// console.log(addone(4));  it will also give output
+// function addone(num){
+//     return num + 1
+// }
+
+
+//  const addtwo = function addone(num){
+//        return num + 2
+//  }
+//  console.log(addtwo(4 It will not give an error    
+ 
+ 
+//  console.log(addtwo(4));  It will give an error    
+ const addtwo = function addone(num){
+       return num + 2
+ }
+
+

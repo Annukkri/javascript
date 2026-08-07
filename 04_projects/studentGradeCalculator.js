b7d88d0d9cb84930384af80bb8f019c8aedad209
Annu Kumari <annustmk@gmail.com>
2026-08-07 07:10:45 +0000

    Add Student Grade Calculator project

---
+++ 04_projects/studentGradeCalculator.js
@@ -0,0 +1,45 @@
+// Student Grade Calculator
+
+let maths = 90;
+let Chemistry = 95;
+let Physics = 80;
+let English = 85;
+let Computer = 75;
+let total = maths + Chemistry + Physics + English + Computer;
+let percentage = (total/500) * 100;
+console.log("Total Marks : " , total);
+console.log("Percentage : " , percentage + "%");
+if (percentage >= 90){
+    console.log("Grade : A+");
+}
+else if (percentage >= 80){
+    console.log("Grade : A");
+}
+else if (percentage >= 70){
+    console.log("Grade : B");
+}
+else if (percentage >= 60){
+    console.log("Grade : C");
+}
+else if (percentage >= 40){
+    console.log("Grade : D");
+}
+else{
+    console.log("Fail");
+}
+    if(percentage >= 40){
+    console.log("Pass");
+    }
+   else{
+    console.log("Fail");
+    }
+
+
+
+
+
+
+
+
+
+    

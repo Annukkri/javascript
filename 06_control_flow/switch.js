2128e98f3136d869dcdf8167608fb7b73c1306f1
Annu Kumari <annustmk@gmail.com>
2026-08-13 08:22:21 +0000

    switch statement in JS

---
+++ 06_control_flow/switch.js
@@ -0,0 +1,32 @@
+// const month = 3
+
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March"); 
+//         break;
+//     default:
+//         console.log("Executed");           
+// }
+
+const month = "march"
+
+switch(month){
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March"); 
+        break;
+    default:
+        console.log("Executed");
+
+}

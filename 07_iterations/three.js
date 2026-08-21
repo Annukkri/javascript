503d308eb2f4b659cd6beaa178c13a7f3e9352ca
Annu Kumari <annustmk@gmail.com>
2026-08-21 09:42:56 +0000

    four.html

--- 07_iterations/three.js
+++ 07_iterations/three.js
@@ -29,6 +29,6 @@ const myObject = {
     game1 : 'NFS',
     game2 : 'Spiderman'
 }
-for (const [key,value] of myObjectobject) {
+for (const [key,value] of myObject) {
     console.log(key, ':-' , value);
 }
